73b7d0ce58ed943af0339318e12673aeda5df7b6
Michael Labbe <mike@frogtoss.com>
2025-04-05 09:38:16 -0700

    upgrade to json_compilation_db 1
    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
 use clap::Parser;
-use std::io::{self, BufRead};
+use std::io::{self, BufWriter};
 use cleanup_compdb::cleanup;
-use json_compilation_db;
+use json_compilation_db::{read, write, Entry};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -12,83 +12,70 @@ struct Args {
     append_arguments: String,
 }
 
-// assumes stdin is good actor; heap attack possible
-#[allow(dead_code)]
-fn read_stdin_until_eof() -> String {
-    let mut buf = String::with_capacity(1024 << 12);
-
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-    
-    while let Some(line) = lines.next() {
-        let line = line.unwrap();
-        buf.push_str(&line);
+fn process_entry(entry: Entry, cli_args: &Args) -> Option<Entry> {
+    if cleanup::is_noncompilation_entry(&entry) {
+        return None;
     }
 
-    buf
+    //
+    // directory
+    let directory = cleanup::enforce_unix_slashes(&entry.directory);
+
+    //
+    // file
+    let mut file = cleanup::absolute_path_to_file(&entry.file, &entry.directory);
+    file = cleanup::enforce_unix_slashes(&file);
+
+    //
+    // output
+    let output = match &entry.output {
+        Some(o) => {
+            let file = cleanup::absolute_path_to_file(&o, &entry.directory);
+            Some(cleanup::enforce_unix_slashes(&file))
+        },
+        _ => None,
+        
+    };
+    
+    //
+    // arguments
+    let mut arguments = cleanup::arguments_strip_cmd_c(&entry.arguments);
+    arguments = cleanup::append_string_to_arguments(&arguments, &cli_args.append_arguments);
+
+    Some(Entry {
+        file,
+        arguments,
+        directory,
+        output,
+    })
+
 }
 
 fn main() {
 
     let cli_args = Args::parse();
-    
-    let entries = match json_compilation_db::from_reader(io::stdin()) {
-        Ok(t) => t,
 
-        Err(e) => {
-            panic!("error: {:?}", e);
-        },
-    };
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let reader = stdin.lock();
+    let writer = BufWriter::new(stdout.lock());
 
-    let mut out_entries: json_compilation_db::Entries = Vec::with_capacity(entries.len());
-
-    for entry in entries.iter() {
-
-        if cleanup::is_noncompilation_entry(&entry) {
-            continue;   
-        }
-        
-        //
-        // directory
-        let directory = cleanup::enforce_unix_slashes(&entry.directory);
-
-        //
-        // file
-        let mut file = cleanup::absolute_path_to_file(&entry.file, &entry.directory);
-        file = cleanup::enforce_unix_slashes(&file);
-
-        //
-        // output
-        let output = match &entry.output {
-            Some(o) => {
-                let file = cleanup::absolute_path_to_file(&o, &entry.directory);
-                Some(cleanup::enforce_unix_slashes(&file))
+    let processed_entries = read(reader).filter_map(|read_result| {
+        match read_result {
+            Ok(entry) => match process_entry(entry, &cli_args) {
+                Some(processed) => Some(processed),
+                None => None
             },
-            _ => None,
+            Err(e) => {
+                eprintln!("read error: {e}");
+                None
+            }
+        }
+    });
 
-        };
-
-        //
-        // arguments
-        let mut arguments = cleanup::arguments_strip_cmd_c(&entry.arguments);
-        arguments = cleanup::append_string_to_arguments(&arguments, &cli_args.append_arguments);
-
-        let out_entry = json_compilation_db::Entry {
-            file: file,
-            arguments: arguments,
-            directory: directory,
-            output: output,
-        };
-
-        out_entries.push(out_entry);
+    if let Err(e) = write(writer, processed_entries) {
+        eprintln!("json database write error: {e}");
+        std::process::exit(1);
     }
 
-    let format = json_compilation_db::Format{
-        command_as_array: true,
-        drop_output_field: false,
-    };
-
-    if let Err(e) = json_compilation_db::to_writer(&out_entries, &format, io::stdout()) {
-        eprintln!("error: {}", e);
-    }
 }
